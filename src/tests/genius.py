777528ce3fa19a40801349dd44e20295cae7179d
TrvsF <traviseff@gmail.com>
2022-11-08 02:05:43 +0000

    api work

--- src/tests/genius.py
+++ src/tests/genius.py
@@ -5,5 +5,17 @@ keyjson = json.load(open("keys.json", "r"))
 key = keyjson["genius-access"]
 
 genius = Genius(key)
-artist = genius.search_artist("Ween", max_songs=3, sort="title")
-print(artist.songs)
+
+songsearch = genius.search_songs("gee beach boy")
+with open('dict.txt', 'w') as file:
+     file.write(json.dumps(songsearch))
+
+for hit in songsearch["hits"]:
+    result = hit["result"]
+    print(result["title"])
+    print(result["artist_names"])
+    print("---")
+
+
+# artist = genius.search_artist("Ween", max_songs=3, sort="title")
+# print(artist.songs)
--- src/tests/guis/simplegui.py
+++ src/tests/guis/simplegui.py
@@ -4,22 +4,29 @@ simplegui.theme("DarkPurple4")
 
 # 1- the layout
 layout = [
-    [
-        simplegui.Text("SongSentimentComparer"),
-    ],
-    [simplegui.Input(key='songinput')],
-    [simplegui.Button('Show'), simplegui.Button('Exit')],
+    [simplegui.Text("SongSentimentComparer")],
+    [simplegui.Input("song name...", key="songinput")],
+    [simplegui.Button("enter")],
     [simplegui.Text(key="output")]
 ]
 
 # 2 - the window
-window = simplegui.Window("SongSentimentComparer", layout, size=(640, 480), element_justification='c')
+window = simplegui.Window("SongSentimentComparer", layout, size=(640, 480), element_justification="c", finalize=True)
+window["songinput"].bind("<Return>", "sireturn")
 
 # 3 - the event loop
 while True:
     event, values = window.read()
-    print(event, values)
-    if event == simplegui.WIN_CLOSED or event == 'Exit':
+    print(event, values)    
+
+    if event == simplegui.WIN_CLOSED:
         break
-    if event == 'Show':
+
+    if event == "Show":
         window["output"].update(values["songinput"])
+
+    if event == "sireturn" + "songinput" or "enter":
+        print(values["songinput"])
+
+    if window.find_element_with_focus() == window["songinput"]:
+        print("a")
