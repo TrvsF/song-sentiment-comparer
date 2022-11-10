996779211dedb7464308588e7a2f7af82ff2c00b
TrvsF <traviseff@gmail.com>
2022-11-10 13:30:53 +0000

    [c]

--- src/tests/cleaner.py
+++ src/tests/cleaner.py
@@ -1,26 +1,25 @@
 import re
 
-with open("Strai.txt", "r") as file:
+with open("Ocean.txt", "r") as file:
     lyrics = file.read()
 
 # get all after word 'lyrics'
 # remove all chars in [] (and the braces)
 # if word contains Embed remove all after \w is satisfied
 
+# remove string seemingly random inserted into some lyric pulls
 lyrics = lyrics.replace("You might also like", "")
 
 # remove the top 'Lyrics'
 lyrics = (re.search(r"(?<=Lyrics)[^*]*", lyrics)).group(0)
+
 # remove the footer embed
-m2 = re.search(r"[\d]*(?=Embed)\w*", lyrics)
+lyrics = re.sub(r"[\d]*(?=Embed)\w*", "", lyrics)
+
 # remove '[]'s
-lyrics = lyrics.replace(m2.group(0), "")
-m3 = re.findall(r"\[.*?\]", lyrics)
+lyrics = re.sub(r"\[.*?\]", "", lyrics)
 
-# [lyrics.replace(group, "") for group in m3]
-for word in m3:
-    lyrics = lyrics.replace(word, "")
-
-# lyrics = (re.search(r"^\n", lyrics)).group(0)
+# clean newlines
+lyrics = re.sub(r"^\n", "", lyrics) # << TODO : only does first line?!??
 
 print(lyrics)
