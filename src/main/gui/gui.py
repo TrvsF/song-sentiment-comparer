09520e9b40c1ef7b38b29ffe2fb47f859a958429
TrvsF <traviseff@gmail.com>
2022-11-22 02:28:43 +0000

    [a] gui kinda suggestions

--- src/main/gui/gui.py
+++ src/main/gui/gui.py
@@ -1,38 +1,112 @@
 #!/usr/bin/env python3
 
 import tkinter as tk
+
 from api.genius import GeniusAPI
 
 class Gui(tk.Tk):
-    def __init__(self, api_obj : GeniusAPI) -> None:
+    def __init__(self, apiobject : GeniusAPI) -> None:
         super().__init__()
 
-        self.geniusapi = api_obj
+        self.genius_api = apiobject
 
         self.title("SSC")
         self.geometry("600x400")
-        self.minsize(340, 200)
+        self.minsize(600, 400)
 
-        self.label = tk.Label(self, text="song sentiment comparer", font=(None, 14))
-        self.label.pack(fill=tk.X, pady=(64, 22))
+        self.header_font = ("System", 22, "bold")
+        self.default_font = ("System", 12)
 
-        self.entry = tk.Entry(self, justify="center", width=52)
-        self.entry.pack(fill=tk.NONE)
+        self.protocol("WM_DELETE_WINDOW", self.safe_destroy)
 
-        self.search = tk.Button(self, text="search", command=self.search)
-        self.search.pack(fill=tk.NONE)
+        self.main_frame = tk.Frame(self,
+            width = 600,
+            height = 400,
+            bg = "lightgrey"
+        )
 
-        self.output = tk.Label(self, text="", font=(None, 14))
-        self.output.pack(fill=tk.NONE, pady=(60, 12))
+        self.title = tk.Label(self.main_frame,
+            text = "song sentiment comparer",
+            bg = "lightgrey",
+            fg = "black",
+            font = self.header_font
+        )
+        self.search_bar = tk.Entry(self.main_frame,
+            bg = "white",
+            fg = "black",
+            width = 52,
+            font = self.default_font
+        )
+        # self.search_button = tk.Button(self.main_frame,
+        #     text = "search",
+        #     bg = "lightgrey",
+        #     fg = "black",
+        #     font = self.default_font,
+        #     command = self.search
+        # )
+        self.recommended_one = tk.Label(self.main_frame,
+            text="ween bananas and blow",
+            bg = "lightgrey",
+            fg = "black",
+            font = self.default_font
+        )
+        self.recommended_two = tk.Label(self.main_frame,
+            text="talking heads im not in love",
+            bg = "lightgrey",
+            fg = "black",
+            font = self.default_font
+        )
+        self.recommended_thr = tk.Label(self.main_frame,
+            text="travis side",
+            bg = "lightgrey",
+            fg = "black",
+            font = self.default_font
+        )
+        self.output_box = tk.Label(self.main_frame,
+            text = "this is an output\nyipeeee",
+            bg = "lightgrey",
+            fg = "black",
+            font = self.default_font
+        )
 
-        self.colour_schemes = [{"bg": "lightgrey", "fg": "black"}, {"bg": "grey", "fg": "white"}]
+        self.main_frame.pack        (fill=tk.BOTH, expand=1)
 
-    def search(self) -> None:
-        searchinput = self.entry.get()
-        artistsongobj = self.geniusapi.get_artistsong_obj_from_search(searchinput)[0]
-        lyrics = self.geniusapi.get_lyrics_from_song(songtitle=artistsongobj["title"], artistname=artistsongobj["artist"])
+        self.title.pack             (fill=tk.X, pady=(32, 28))
+        self.search_bar.pack        (fill=tk.NONE)
+        self.recommended_one.pack   (fill=tk.NONE)
+        self.recommended_two.pack   (fill=tk.NONE)
+        self.recommended_thr.pack   (fill=tk.NONE)
+        # self.search_button.pack (fill=tk.NONE)
+        self.output_box.pack        (fill=tk.BOTH, pady=(22, 12))
 
-        self.output["text"] = lyrics
+        self.recommended_one.bind("<Button-1>", lambda eff: self.search(eff, search_text=self.recommended_one["text"]))
+        self.recommended_two.bind("<Button-1>", lambda eff: self.search(eff, search_text=self.recommended_two["text"]))
+        self.recommended_thr.bind("<Button-1>", lambda eff: self.search(eff, search_text=self.recommended_thr["text"]))
+        self.bind("<Return>", self.search)
+
+        self.search_bar.focus()
+
+    def search(self, event = None, search_text = "") -> None:
+        artistsongobj = None
+
+        if search_text == "":
+            searchinput = self.search_bar.get()
+            artistsongobj = self.genius_api.get_artistsong_obj_from_search(searchinput)[0]
+        else:
+            artistsongobj = self.genius_api.get_artistsong_obj_from_search(search_text)[0]
+
+        if artistsongobj == None:
+            print("artist obj was none")
+            return
+
+        lyrics = self.genius_api.get_lyrics_from_song(songtitle=artistsongobj["title"], artistname=artistsongobj["artist"])
+
+        self.output_box["text"] = lyrics
+
+    def safe_destroy(self) -> None:
+        # check if model is running & stop
+        print("deading...")
+        self.destroy()
 
 if __name__ == "__main__":
     root = Gui()
